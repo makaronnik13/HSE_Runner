0f32769d8a5f7f4e24a96c9023b6ba26aadd0f9c
Makaronnik13 <13nik@mail.ru>
2024-08-27 20:02:07 +0300

    build update

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "vintorog-CampusRunner-0.0.21";
-const contentToCache = [
-    "Build/HSE_Runner.loader.js",
-    "Build/HSE_Runner.framework.js.br",
-    "Build/HSE_Runner.data.br",
-    "Build/HSE_Runner.wasm.br",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
