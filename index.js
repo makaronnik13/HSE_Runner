b49e7636b73e7c0435ea64dd5cd7edb5c9a19aca
Makaronnik13 <13nik@mail.ru>
2024-09-27 17:07:13 +0300

    telegram user info

--- index.js
+++ index.js
@@ -1,51 +1,98 @@
-const express = require("express");
-const path = require("path");
-const TelegramBot = require("node-telegram-bot-api");
-const TOKEN = "7274247476:AAE2XQinh0epCZr53DlCxndtTynkIk8LdcU";
-const server = express();
-const bot = new TelegramBot(TOKEN, {
-    polling: true
-});
-const port = process.env.PORT || 5000;
-const gameName = "hse_runner";
-const queries = {};
-server.use(express.static(path.join(__dirname, 'web_8_22_v_0.0.20')));
-bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "Say /game if you want to play."));
-bot.onText(/start|game/, (msg) => bot.sendGame(msg.from.id, gameName));
-bot.on("callback_query", function (query) {
-    if (query.game_short_name !== gameName) {
-        bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
-    } else {
-        queries[query.id] = query;
-        let gameurl = "https://makaronnik13.github.io/HSE_Runner/";
-        bot.answerCallbackQuery({
-            callback_query_id: query.id,
-            url: gameurl
-        });
+window.addEventListener("load", function () {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("ServiceWorker.js");
     }
-});
-bot.on("inline_query", function (iq) {
-    bot.answerInlineQuery(iq.id, [{
-        type: "game",
-        id: "0",
-        game_short_name: gameName
-    }]);
-});
-server.get("/highscore/:score", function (req, res, next) {
-    if (!Object.hasOwnProperty.call(queries, req.query.id)) return next();
-    let query = queries[req.query.id];
-    let options;
-    if (query.message) {
-        options = {
-            chat_id: query.message.chat.id,
-            message_id: query.message.message_id
-        };
-    } else {
-        options = {
-            inline_message_id: query.inline_message_id
-        };
+  });
+  var unityInstanceRef;
+  var unsubscribe;
+  var container = document.querySelector("#unity-container");
+  var canvas = document.querySelector("#unity-canvas");
+  var loadingBar = document.querySelector("#unity-loading-bar");
+  var progressBarFull = document.querySelector("#unity-progress-bar-full");
+  var warningBanner = document.querySelector("#unity-warning");
+
+  const overflow = 1
+document.body.style.overflowY = 'hidden'
+document.body.style.marginTop = `${overflow}px`
+document.body.style.height = window.innerHeight + overflow + "px"
+document.body.style.paddingBottom = `${overflow}px`
+window.scrollTo(0, overflow)
+
+
+  var tg = window.Telegram.WebApp;
+  tg.ready();
+  tg.expand();
+  var initData = tg.initData;
+  var userData = JSON.stringify(initData, null, 2);
+  console.log(userData);
+
+  // Shows a temporary message banner/ribbon for a few seconds, or
+  // a permanent error message on top of the canvas if type=='error'.
+  // If type=='warning', a yellow highlight color is used.
+  // Modify or remove this function to customize the visually presented
+  // way that non-critical warnings and error messages are presented to the
+  // user.
+  function unityShowBanner(msg, type) {
+    function updateBannerVisibility() {
+      warningBanner.style.display = warningBanner.children.length ? 'block' : 'none';
     }
-    bot.setGameScore(query.from.id, parseInt(req.params.score), options,
-        function (err, result) {});
-});
-server.listen(port);
+    var div = document.createElement('div');
+    div.innerHTML = msg;
+    warningBanner.appendChild(div);
+    if (type == 'error') div.style = 'background: red; padding: 10px;';
+    else {
+      if (type == 'warning') div.style = 'background: yellow; padding: 10px;';
+      setTimeout(function() {
+        warningBanner.removeChild(div);
+        updateBannerVisibility();
+      }, 5000);
+    }
+    updateBannerVisibility();
+  }
+
+  var buildUrl = "Build";
+  var loaderUrl = buildUrl + "/HSE_Runner.loader.js";
+  var config = {
+    dataUrl: buildUrl + "/HSE_Runner.data",
+    frameworkUrl: buildUrl + "/HSE_Runner.framework.js",
+    codeUrl: buildUrl + "/HSE_Runner.wasm",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "vintorog",
+    productName: "CampusRunner",
+    productVersion: "0.0.25",
+    showBanner: unityShowBanner,
+  };
+
+  // By default Unity keeps WebGL canvas render target size matched with
+  // the DOM size of the canvas element (scaled by window.devicePixelRatio)
+  // Set this to false if you want to decouple this synchronization from
+  // happening inside the engine, and you would instead like to size up
+  // the canvas DOM size and WebGL render target sizes yourself.
+  // config.matchWebGLToCanvasSize = false;
+
+
+  if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+    // Mobile device style: fill the whole browser client area with the game canvas:
+    var meta = document.createElement('meta');
+    meta.name = 'viewport';
+    meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
+    document.getElementsByTagName('head')[0].appendChild(meta);
+  }
+
+  loadingBar.style.display = "block";
+
+  var script = document.createElement("script");
+  script.src = loaderUrl;
+  script.onload = () => {
+    createUnityInstance(canvas, config, (progress) => {
+      progressBarFull.style.width = 100 * progress + "%";
+    }).then((unityInstance) => {
+      unityInstanceRef = unityInstance;
+      loadingBar.style.display = "none";
+      //unityInstanceRef.SendMessage('PayloadManager', 'OnTelegramInitData', userData);
+    }).catch((message) => {
+      console.error(message);
+      window.location.reload();
+    });
+  };
+  document.body.appendChild(script);
